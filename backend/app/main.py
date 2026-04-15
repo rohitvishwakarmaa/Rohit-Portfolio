ad143614b6e43ff41e28c80b5dd7f338327d9ad2
Rohit Vishwakarma <rohitvishwakarmaarv@gmail.com>
2026-04-15 23:32:14 +0530

    fix: radically simplify CORS regex for vercel domains

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,7 +55,7 @@ allowed_origins = [str(o) for o in settings.BACKEND_CORS_ORIGINS] if settings.BA
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
-    allow_origin_regex=r"^https?://(.*\.vercel\.app|localhost:\d+|(www\.)?rohitvishwakarma\.com)$",
+    allow_origin_regex=r"https://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
